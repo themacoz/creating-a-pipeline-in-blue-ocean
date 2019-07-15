a1a83dcc9bff54ea6ac233b99612efde631df204
themacoz <themacoz@gmail.com>
2019-07-15 15:56:26 -0400

    Changing to see if pipeline is triggered
    The other tests edited these files through Blue ocean

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+# seeing what happens if I edit this script
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
